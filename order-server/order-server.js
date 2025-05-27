789d8b34405e450284eca1a2a545142d001e48e4
Ayman-AbuHijleh <a2y2m2a2n@gmail.com>
2025-05-27 21:24:58 +0300

    commit two for order server

--- order-server/order-server.js
+++ order-server/order-server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const csvParser = require('csv-parser');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const app = express();
 const PORT = 3002;
+const orderFile = './orders.csv';
 const bookFile = './proj.csv';
 
 app.use(express.json());
@@ -19,6 +21,35 @@ function readBooks() {
             .on('end', () => resolve(books))
             .on('error', (error) => reject(error));
     });
+}
+
+function writeBooks(books) {
+    const csvWriter = createCsvWriter({
+        path: bookFile,
+        header: [
+            { id: 'id', title: 'id' },
+            { id: 'title', title: 'title' },
+            { id: 'author', title: 'author' },
+            { id: 'topic', title: 'topic' },
+            { id: 'price', title: 'price' },
+            { id: 'stock', title: 'stock' },
+        ],
+    });
+    return csvWriter.writeRecords(books);
+}
+
+function logOrder(order) {
+    const csvWriter = createCsvWriter({
+        path: orderFile,
+        header: [
+            { id: 'order_id', title: 'order_id' },
+            { id: 'item_id', title: 'item_id' },
+            { id: 'title', title: 'title' },
+            { id: 'quantity', title: 'quantity' },
+        ],
+        append: true,
+    });
+    return csvWriter.writeRecords([order]);
 }
 
 app.listen(PORT, () => {
